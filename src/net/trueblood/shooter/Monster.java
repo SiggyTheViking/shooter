9e37f1c02f87719665b9221928632cab24ef094b
Darin Trueblood <darin.trueblood@gmail.com>
2010-08-29 08:39:33 -0700

    added location to monster, doDammage returns double
    added extra constructor to monster with location

--- src/net/trueblood/shooter/Monster.java
+++ src/net/trueblood/shooter/Monster.java
@@ -3,12 +3,20 @@ package net.trueblood.shooter;
 public class Monster {
 	private String _name;
 	private int _hitPoints;
+	private final int _fullHealth;
 	private boolean _isAlive = true;
-	//private int _location;
+	private int _location = 0;
 	
 	public Monster(String name, int hp){
 		_name = name;
 		_hitPoints = hp;
+		_fullHealth = hp;
+	}
+	public Monster(String name, int hp, int location){
+		_name = name;
+		_hitPoints = hp;
+		_location = location;
+		_fullHealth = hp;
 	}
 	
 	public String getName(){
@@ -17,15 +25,26 @@ public class Monster {
 	public int getHitPoints(){
 		return _hitPoints;
 	}
+	public int getLocation(){
+		return _location;
+	}
+	public void setLocation(int location){
+		_location = location;
+	}
 	
 	//returns true if monster is still alive,
 	//false if killed
-	public boolean DoDammage(int dammage){
+	public double DoDammage(int dammage){
 		_hitPoints = _hitPoints - dammage;
 		if (_hitPoints < 1){
 			_isAlive = false;
 		}
-		return _isAlive;
+		if (_hitPoints == 0){
+			return 0;
+		}
+		else{
+			return _hitPoints / _fullHealth;
+		}
 	}
 
 }
